3228c6f31ddf6936f6c7e988ce0952b77b9b09d7
Changhwan Kim <kimch061279@gmail.com>
2023-06-24 01:01:11 +0900

    파일 삭제 기능 준비

--- lib/main.dart
+++ lib/main.dart
@@ -86,8 +86,7 @@ class HomePage extends StatelessWidget {
                                 ListTile(
                                     title: Text("삭제"),
                                     onTap: () {
-                                      controller.sortBy(SortBy.date);
-                                      Navigator.pop(context);
+                                      delFolder(context);
                                     }),
                                 ListTile(
                                     title: Text("이름 변경"),
@@ -231,6 +230,45 @@ class HomePage extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+
+  delFolder(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController folderName = TextEditingController();
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  title: TextField(
+                    controller: folderName,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      // Create Folder
+                      await FileManager.createFolder(
+                          controller.getCurrentPath, folderName.text);
+                      // Open Created Folder
+                      controller.setCurrentPath =
+                          controller.getCurrentPath + "/" + folderName.text;
+                    } catch (e) {}
+
+                    Navigator.pop(context);
+                  },
+                  child: Text('새 폴더 생성'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
